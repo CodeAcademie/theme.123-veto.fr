e40ce8b14dbe8f4fc4c569f7843e60e20a2a99bc
erwannFace <e.duclos@fondationface.org>
2018-01-02 08:15:48 +0100

    Adding son style

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
     //ça en ligne 1 sinon ça plante.
 ?>
 
     <div id="footer">
         <p> Copyright &#169;
-            <?php print(date(Y)); ?>
+            <?php print(date("Y")); ?>
                 <?php bloginfo('name'); ?>
                     <br /> Site propulsé par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par La Team 123VETO
 
@@ -37,6 +37,5 @@
         </div>
     </div>
 
-    
-   <?php wp_footer(); ?>
 
+   <?php wp_footer(); ?>
